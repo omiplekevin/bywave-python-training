96fa2f70f72c1ee612b33aa1c5b4dc4164008ca9
xXLXx <leo.lavarias@gmail.com>
2018-10-16 15:54:38 +0800

    Add last exercises for slide 1 & 2

---
+++ exer_004.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Exercise #4
+
+Write a program that prints out the square of the first 20 integers
+in a block such that the block has a dimension of 4x5.
+
+ 1   4   9   16
+ 25  36  49  64
+ 81 100 121 144
+169 196 225 256
+289 324 361 400
+"""
+
+
+# your name and email address here
+__author__ = 'xXLXx <leo@bywave.com.au>'
+
+
+if __name__ == '__main__':
+    # your code here
